481623b37d65327c9ec120dbe6607246781f130d
patrickruiz1 <pruiz7@me.com>
2024-05-02 04:00:37 -0700

    2may24, night
    worked on testing analysis, and calibration curve analysis

--- src/data_analysis/calibration_curvefitting.py
+++ src/data_analysis/calibration_curvefitting.py
@@ -17,11 +17,10 @@ def power_function(x, a, b):
     """
     return a * np.power(x, b)
 
-
-def calibration_curvefitting(FSR_dir, file_name):
+def calibration_curvefitting(FSR_dir, file_name, graph_title = 'Graph'):
 
     # File and directory information
-    file_path = os.path.join(os.getcwd(), 'data', FSR_dir, 'processed', file_name)
+    file_path = os.path.join(os.getcwd(), 'data', FSR_dir, 'calibration', file_name)
 
     # Read data from CSV file
     df = pd.read_csv(file_path, index_col=False)
@@ -37,14 +36,10 @@ def calibration_curvefitting(FSR_dir, file_name):
     SE_A = SE[0]
     SE_B = SE[1]
 
-    # Print parameter estimates and standard errors
-    print("Parameter Estimates and Standard Errors:")
-    print(f"The value of A is {a_fit:.5f} with standard error of {SE_A:.5f}.")
-    print(f"The value of B is {b_fit:.5f} with standard error of {SE_B:.5f}.")
-
     # Generate fitted curve
     x_fit = np.linspace(min(x_data), max(x_data), 3000)
     y_fit = power_function(x_fit, a_fit, b_fit)
+    z_fit = (1/y_fit)
 
     # Calculate confidence intervals based on x_fit
     alpha = 0.05
@@ -53,12 +48,6 @@ def calibration_curvefitting(FSR_dir, file_name):
     t = np.abs(np.random.standard_t(df_resid, size=len(x_fit)))
     s_err = np.sqrt(np.sum((y_data - power_function(x_data, a_fit, b_fit)) ** 2) / df_resid)
     CI = t * s_err * np.sqrt(1/n + (x_fit - np.mean(x_data))**2 / np.sum((x_data - np.mean(x_data))**2))
-
-    # Print values for LaTeX document
-    print("\nValues for LaTeX Document:")
-    print(f"\\textbf{{Parameter Estimates and Standard Errors:}}")
-    print(f"The value of $A$ is {a_fit:.5f} with standard error of {SE_A:.5f}.")
-    print(f"The value of $B$ is {b_fit:.5f} with standard error of {SE_B:.5f}.")
 
     # Calculate R-squared
     y_pred = power_function(x_data, a_fit, b_fit)
@@ -69,37 +58,49 @@ def calibration_curvefitting(FSR_dir, file_name):
     # Calculate Mean Squared Error (MSE)
     mse = np.mean((y_data - y_pred)**2)
 
-    print("\n\\textbf{Goodness-of-Fit Metrics:}")
-    print(f"Coefficient of Determination ($R^2$): {r_squared:.5f}")
-    print(f"Mean Squared Error (MSE): {mse:.5f}")
-
     # Calculate residuals
     residuals = y_data - y_pred
 
-    print("\n\\textbf{Residuals:}")
-    print(residuals)
-
     # Plotting
-    plt.figure(figsize=(10, 6))
-    plt.plot(x_data, y_data, c='black', marker=',', label='Original Data')
-    plt.plot(x_fit, y_fit, c='blue', label='Fitted Curve')
-    plt.fill_between(x_fit, y_fit - 2*CI, y_fit + 2*CI, color='black', alpha=.69, label='95% Confidence Interval')
-
-    # Add equation text to the plot
-    equation_text = f'$y = {a_fit:.2f} \cdot x^{{{b_fit:.2f}}}$'
-    plt.text(max(x_data)*0.6, max(y_data)*0.7, equation_text, fontsize=12, color='black')
+    fig = plt.figure(figsize = (11,7))
+    ax1 = fig.add_subplot(111)
+    lns1 = ax1.scatter(x_data, y_data, c = 'black', marker = '.', label = 'Original Data')
+    lns2 = ax1.plot(x_fit, y_fit, c = 'blue', label = 'Fitted Curve')
+    ax2 = ax1.twinx()
+    lns3 = ax2.plot(x_fit, z_fit, c = 'orange', label = 'Conductance')
 
     # Add labels, title, legend, and grid
     plt.xlabel('Force (lbf)')
-    plt.ylabel('Resistance (Ohms)')
-    plt.title(file_name[:-4])
-    plt.legend()
+    ax1.set_ylabel(r'Resistance ($\Omega$)')
+    ax1.set_xlabel('Force (lbf)')
+    ax2.set_ylabel(r'Conductance (S)')
+    plt.title(graph_title)
+    lns = [lns1] + lns2 + lns3
+    labs = [l.get_label() for l in lns]
+    ax1.legend(lns, labs, loc = 'upper center')
     plt.grid(True)
 
     # Show plot
     plt.show()
 
-os.system('cls')
+    # Print values for LaTex document 
+    print("Fitted Function Equation:")
+    print(f"The fitted function equation is: $y = {a_fit:.5f} \\times x^{{{b_fit:.5f}}}$") 
+
+    print(f"\nParameter Estimates and Standard Errors:")
+    print(f"The value of $A$ is {a_fit:.5f} with standard error of {SE_A:.5f}.")
+    print(f"The value of $B$ is {b_fit:.5f} with standard error of {SE_B:.5f}.")
+
+    print("\nGoodness-of-Fit Metrics:")
+    print(f"Coefficient of Determination ($R^2$): {r_squared:.5f}")
+    print(f"Mean Squared Error (MSE): {mse:.5f}")
+
+    print("\nResiduals:")
+    # np.set_printoptions(threshold=np.inf)
+    print(residuals)
+
+os.system('clear')
 FSR_dir = 'FSR_S1'
-file_name = 'FSR_S1_Calibration_PreCond.csv'
-calibration_curvefitting(FSR_dir, file_name)
+file_name = 'FSR_S1_Calibration_PostCond_Swag.csv'
+graph_title = 'FSR_S1 - Calibration Curve - Post Conditioning'
+calibration_curvefitting(FSR_dir, file_name, graph_title)
---
+++ src/data_analysis/test_analysis.py
@@ -0,0 +1,67 @@
+import os 
+import numpy as np
+import pandas as pd 
+
+def system_function(x, a = 12964.65477, b = -0.90492):
+    return a * np.power(x, b)
+
+def relative_error(actual, experimental):
+    return (abs(actual - experimental)/actual)*100
+
+def calculate_rmse(actual_values, estimated_values):
+    act, exp = np.array(actual_values), np.array(estimated_values)
+    squared_errors = (act - exp) ** 2
+    rmse = np.sqrt(np.mean(squared_errors))
+    return rmse
+
+def calculate_mae(actual_values, estimated_values):
+    act, exp = np.array(actual_values), np.array(estimated_values)
+    absolute_errors = np.abs(act - exp)
+    mae = np.mean(absolute_errors)
+    return mae
+
+def test_analysis(FSR_dir, file_name):
+    file_path = os.path.join(os.getcwd(), 'data', FSR_dir, 'test', file_name)
+
+    df = pd.read_csv(file_path, index_col = False)
+
+    data = {}
+
+    flag, new_data, counter = False, [], 0
+    for index, row in df.iterrows():
+        if row.iloc[0] > 4:
+            flag = True
+            new_data.append([float(row.iloc[0]), float(row.iloc[1])])
+        else:
+            if flag == True:
+                data[counter] = new_data
+                counter = counter + 1
+                flag = False
+                new_data = []
+            else:
+                pass
+    
+    new_data = {}
+    for i in list(data.keys()):
+        actual, experimental = [], []
+        for j in data[i]:
+            j.append(round(system_function(float(j[0])), 5))
+            actual.append(j[1])
+            experimental.append(j[2])
+
+        rsme = round(calculate_rmse(actual, experimental), 5)
+        mae = round(calculate_mae(actual, experimental), 5)
+        new_data[i] = data[i], rsme, mae
+        
+    for i in list(new_data.keys()):
+        print(f'RSME = {new_data[i][1]}, MAE = {new_data[i][2]}')
+
+
+            
+
+
+
+os.system('clear')
+FSR_dir = 'FSR_S1'
+file_name = 'FSR_S1_Cond_Swag' + '.csv'
+test_analysis(FSR_dir, file_name)
